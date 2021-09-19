bbd7d54717274314c2edabe5bd240d7dfce83e19
David Scott <dave@recoil.org>
2021-09-19 11:23:50 +0100

    Periodically print status for debugging
    Signed-off-by: David Scott <dave@recoil.org>

--- cgroup/cmd/load/main.go
+++ cgroup/cmd/load/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
@@ -14,14 +17,13 @@ import (
 
 var (
 	// Paths to pin objects in /sys/fs/bpf:
-	ingressProgramPath    = "/sys/fs/bpf/ingress_program"
-	egressProgramPath     = "/sys/fs/bpf/egress_program"
-	blockedMapPath        = "/sys/fs/bpf/blocked_map"
+	blockedMapPath = "/sys/fs/bpf/blocked_map"
 
-	// Symbols from bpf.c
+	// Symbols from bpf.c:
 	ingressProgram = "ingress"
 	egressProgram  = "egress"
 	blockedMap     = "blocked_map"
+	flowsMap       = "flows_map"
 )
 
 func main() {
@@ -39,18 +41,12 @@ func main() {
 		panic(err)
 	}
 
-	if err := c.Programs[ingressProgram].Pin(ingressProgramPath); err != nil{
-		panic(err)
-	}
+	// The links to the cgroup will be removed when the program exits.
 	if _, err = link.AttachCgroup(link.CgroupOptions{
 		Path:    *cgroup,
 		Attach:  ebpf.AttachCGroupInetIngress,
 		Program: c.Programs[ingressProgram],
 	}); err != nil {
-		panic(err)
-	}
-
-	if err := c.Programs[egressProgram].Pin(egressProgramPath); err != nil{
 		panic(err)
 	}
 	if _, err := link.AttachCgroup(link.CgroupOptions{
@@ -61,6 +57,10 @@ func main() {
 		panic(err)
 	}
 
+	// A separate program needs to access the shared map so pin it to the fs.
+	if err := os.Remove(blockedMapPath); err != nil && !os.IsNotExist(err) {
+		panic(err)
+	}
 	if err := c.Maps[blockedMap].Pin(blockedMapPath); err != nil {
 		panic(err)
 	}
@@ -70,15 +70,52 @@ func main() {
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
 	fmt.Println("eBPF loaded and active. Hit Control+C to unload and exit.")
-	<-sigc
+	// Display recent decisions for debugging:
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			var conn conn
+			for c.Maps[flowsMap].LookupAndDelete(nil, &conn) == nil {
+				fmt.Println(unmarshalFlow(conn))
+			}
+		case <-sigc:
+			return
+		}
+	}
 
-	if err := os.Remove(ingressProgramPath); err != nil {
-		fmt.Printf("removing %s: %v\n", ingressProgramPath, err)
+}
+
+// Matches definition in bpf.c
+type conn struct {
+	Flags uint32
+	Dst   uint32
+	Src   uint32
+}
+
+// Higher-level Go version
+type flow struct {
+	Blocked bool
+	Egress  bool
+	Src     net.IP
+	Dst     net.IP
+}
+
+func (f flow) String() string {
+	return fmt.Sprintf("{ Src = %s, Dst = %s, Egress = %v, Blocked = %v }", f.Src, f.Dst, f.Egress, f.Blocked)
+}
+
+func unmarshalFlow(c conn) flow {
+	return flow{
+		Blocked: c.Flags&2 != 0,
+		Egress:  c.Flags&1 != 0,
+		Src:     unmarshalIPv4(c.Src),
+		Dst:     unmarshalIPv4(c.Dst),
 	}
-	if err := os.Remove(egressProgramPath); err != nil {
-		fmt.Printf("removing %s: %v\n", egressProgramPath, err)
-	}
-	if err := os.Remove(blockedMapPath); err != nil {
-		fmt.Printf("removing %s: %v\n", blockedMapPath, err)
-	}
+}
+
+func unmarshalIPv4(ipv4 uint32) net.IP {
+	var b [4]byte
+	binary.LittleEndian.PutUint32(b[:], ipv4)
+	return net.IPv4(b[0], b[1], b[2], b[3])
 }
